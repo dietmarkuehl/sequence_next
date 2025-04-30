64cca3500ee5e25f9a104bb7e22f5ca1821158f1
Dietmar Kühl <dietmar.kuehl@me.com>
2025-04-30 11:54:27 -0600

    clang formatted all files

--- examples/hello-sequence_next.cpp
+++ examples/hello-sequence_next.cpp
@@ -10,17 +10,12 @@ namespace sn = beman::sequence_next;
 // ----------------------------------------------------------------------------
 
 int main() {
-    ex::sync_wait(
-          sn::iota{2, 5}
-        //| something(resume_after(100ms))
-        | sn::then_each([](auto x){
-            std::cout << "element1=" << x << '\n';
-            return x * 2;
-            })
-        | sn::then_each([](auto x){
-            std::cout << "element2=" << x << '\n';
-            })
-        | sn::ignore_all);
+    ex::sync_wait(sn::iota{2, 5} //| something(resume_after(100ms))
+                  | sn::then_each([](auto x) {
+                        std::cout << "element1=" << x << '\n';
+                        return x * 2;
+                    }) |
+                  sn::then_each([](auto x) { std::cout << "element2=" << x << '\n'; }) | sn::ignore_all);
 
     for (int i{}; i != 5; ++i) {
         ex::sync_wait(sn::conditional_element(ex::just(i, 2),
