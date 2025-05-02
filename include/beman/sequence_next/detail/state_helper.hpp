3763c4191eea8e3cbbfcce58ae5ce5c7c6fbf04f
Dietmar Kühl <dietmar.kuehl@me.com>
2025-05-02 09:31:51 -0600

    clang format

--- include/beman/sequence_next/detail/state_helper.hpp
+++ include/beman/sequence_next/detail/state_helper.hpp
@@ -18,10 +18,8 @@ struct state_helper {
 
     state_t state;
 
-    state_helper(S&& s, R&& r) : state(::beman::execution::connect(::std::forward<S>(s), ::std::forward<R>(r))) {
-    }
-    ~state_helper() {
-    }
+    state_helper(S&& s, R&& r) : state(::beman::execution::connect(::std::forward<S>(s), ::std::forward<R>(r))) {}
+    ~state_helper() {}
 
     auto start() & noexcept { ::beman::execution::start(this->state); }
 };
